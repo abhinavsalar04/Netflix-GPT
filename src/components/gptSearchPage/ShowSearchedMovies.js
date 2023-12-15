65fb331155c3a6cc17d9b6289f467c99171aef0f
abhinavsalar18 <abhinav.2021ca006@mnnit.ac.in>
2023-12-15 22:46:56 +0530

    Shimmer UI changes, bug fixed

--- src/components/gptSearchPage/ShowSearchedMovies.js
+++ src/components/gptSearchPage/ShowSearchedMovies.js
@@ -3,16 +3,17 @@ import MovieList from "../homePage/MovieList";
 
 const ShowSearchedMovies = () => {
    const {movieSearchResults, searchedMovieName} = useSelector((store) => store.movies);
-    console.log(movieSearchResults);
-    if(!searchedMovieName || movieSearchResults.total_results === 0)
-      return (
-        <>
-            <h2 className="text-[#ff0026] flex justify-center text-2xl  bg-[rgb(51,51,51)] bg-opacity-70 px-4 mx-auto items-center mt-4 rounded">No result found!</h2>
-            <div  className=" pt-[220px] w-screen text-[cyan]">
-            </div>
-        </>
-      );
+    // console.log(movieSearchResults);
+    if(!searchedMovieName) return <div  className=" pt-[270px] w-screen">
+    </div>
 
+    if(searchedMovieName && movieSearchResults.total_results === 0)
+    return  <>
+        <h2 className={`text-[#ff0026] flex justify-center text-2xl  bg-[rgb(51,51,51)] bg-opacity-70 px-4 mx-auto items-center mt-4 rounded`}>No result found!</h2>
+        <div  className=" pt-[220px] w-screen text-[cyan]">
+        </div>
+    </>
+      
     return (
         <div className="p-4 m-4 bg-black text-white rounded-md justify-center bg-opacity-70">
             <div>    
